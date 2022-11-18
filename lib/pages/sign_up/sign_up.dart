8755be0f774874c7e0904c8397dfef5d65eaa0a0
Daffa Ilhami <mdaffailhami@gmail.com>
2022-11-18 14:06:47 +0800

    Menghapus (comment) Google Sign In

--- lib/pages/sign_up/sign_up.dart
+++ lib/pages/sign_up/sign_up.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:marketku/widgets/marketku_logotype.dart';
 import 'package:marketku/widgets/text_form_field.dart';
 
-class MySignUpPage extends StatelessWidget {
+class MySignUpPage extends StatefulWidget {
   const MySignUpPage({super.key});
+
+  @override
+  State<MySignUpPage> createState() => _MySignUpPageState();
+}
+
+class _MySignUpPageState extends State<MySignUpPage> {
+  var nama = '';
+  var email = '';
+  var password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -32,22 +43,22 @@ class MySignUpPage extends StatelessWidget {
                           children: [
                             Divider(),
                             MyTextFormField(
-                              onChanged: (String value) {},
                               labelText: 'Nama',
                               hintText: 'Masukkan nama anda',
+                              onChanged: (String value) => nama = value,
                             ),
                             SizedBox(height: 10),
                             MyTextFormField(
-                              onChanged: (String value) {},
                               labelText: 'Email',
                               hintText: 'Masukkan alamat email anda',
+                              onChanged: (String value) => email = value,
                             ),
                             SizedBox(height: 10),
                             MyTextFormField(
-                              onChanged: (String value) {},
                               labelText: 'Password',
                               hintText: 'Masukkan password yang kuat!',
                               obscureText: true,
+                              onChanged: (String value) => password = value,
                             ),
                             SizedBox(height: 12),
                             TextButton(
@@ -70,80 +81,82 @@ class MySignUpPage extends StatelessWidget {
                               ),
                               child: Text('Daftar'),
                             ),
-                            SizedBox(height: 2),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Divider(indent: 5, endIndent: 10.0),
-                                ),
-                                Text(
-                                  "ATAU",
-                                  style: TextStyle(
-                                      color: Theme.of(context).brightness ==
-                                              Brightness.dark
-                                          ? Theme.of(context)
-                                              .colorScheme
-                                              .outline
-                                          : Colors.grey,
-                                      fontSize: 12),
-                                ),
-                                Expanded(
-                                  child: Divider(indent: 10.0, endIndent: 5),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 2),
-                            TextButton(
-                              onPressed: () {},
-                              style: TextButton.styleFrom(
-                                backgroundColor: Theme.of(context)
-                                    .colorScheme
-                                    .surfaceVariant
-                                    .withOpacity(0.7),
-                                foregroundColor: Theme.of(context)
-                                    .colorScheme
-                                    .onSurfaceVariant,
-                                fixedSize: Size.fromWidth(
-                                    MediaQuery.of(context).size.width),
-                              ),
-                              child: Stack(
-                                alignment: Alignment.centerLeft,
+                            // SizedBox(height: 2),
+                            // Row(
+                            //   children: [
+                            //     Expanded(
+                            //       child: Divider(indent: 5, endIndent: 10.0),
+                            //     ),
+                            //     Text(
+                            //       "ATAU",
+                            //       style: TextStyle(
+                            //           color: Theme.of(context).brightness ==
+                            //                   Brightness.dark
+                            //               ? Theme.of(context)
+                            //                   .colorScheme
+                            //                   .outline
+                            //               : Colors.grey,
+                            //           fontSize: 12),
+                            //     ),
+                            //     Expanded(
+                            //       child: Divider(indent: 10.0, endIndent: 5),
+                            //     ),
+                            //   ],
+                            // ),
+                            // SizedBox(height: 2),
+                            // TextButton(
+                            //   onPressed: () {},
+                            //   style: TextButton.styleFrom(
+                            //     backgroundColor: Theme.of(context)
+                            //         .colorScheme
+                            //         .surfaceVariant
+                            //         .withOpacity(0.7),
+                            //     foregroundColor: Theme.of(context)
+                            //         .colorScheme
+                            //         .onSurfaceVariant,
+                            //     fixedSize: Size.fromWidth(
+                            //         MediaQuery.of(context).size.width),
+                            //   ),
+                            //   child: Stack(
+                            //     alignment: Alignment.centerLeft,
+                            //     children: [
+                            //       SizedBox(
+                            //         width: double.infinity,
+                            //         child: Text(
+                            //           'Daftar dengan Google',
+                            //           textAlign: TextAlign.center,
+                            //           style: TextStyle(
+                            //             color: Theme.of(context)
+                            //                 .colorScheme
+                            //                 .primary,
+                            //           ),
+                            //         ),
+                            //       ),
+                            //       SvgPicture.asset('assets/google.svg',
+                            //           width: 26),
+                            //     ],
+                            //   ),
+                            // ),
+                            SizedBox(height: 16),
+                            RichText(
+                              text: TextSpan(
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.outline),
                                 children: [
-                                  SizedBox(
-                                    width: double.infinity,
-                                    child: Text(
-                                      'Daftar dengan Google',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primary,
-                                      ),
+                                  TextSpan(text: 'Sudah punya akun? '),
+                                  TextSpan(
+                                    text: 'Masuk',
+                                    style: TextStyle(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      fontWeight: FontWeight.w500,
                                     ),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () => print('MASUK'),
                                   ),
-                                  SvgPicture.asset('assets/google.svg',
-                                      width: 26),
                                 ],
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      RichText(
-                        text: TextSpan(
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.outline),
-                          children: [
-                            TextSpan(text: 'Sudah punya akun? '),
-                            TextSpan(
-                              text: 'Masuk',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.primary,
-                                fontWeight: FontWeight.w500,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () => print('MASUK'),
                             ),
                           ],
                         ),
