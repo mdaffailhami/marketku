7633fc95549faa74332d92c580396924aaa6c312
Daffa Ilhami <mdaffailhami@gmail.com>
2022-11-19 13:26:54 +0800

    Create nav_bar.dart

---
+++ lib/pages/home/nav_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MyNavBar extends StatelessWidget {
+  const MyNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+  });
+
+  final int selectedIndex;
+  final void Function(int) onDestinationSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+            width: 0.2,
+          ),
+        ),
+      ),
+      child: NavigationBar(
+        surfaceTintColor: Theme.of(context).colorScheme.surface,
+        height: 66,
+        selectedIndex: selectedIndex,
+        onDestinationSelected: onDestinationSelected,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home_filled),
+            icon: Icon(Icons.home_outlined),
+            label: 'Beranda',
+            tooltip: '',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.storefront_rounded),
+            icon: Icon(Icons.storefront),
+            label: 'MarketKu',
+            tooltip: '',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.message),
+            icon: Icon(Icons.message_outlined),
+            label: 'Pesan',
+            tooltip: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
