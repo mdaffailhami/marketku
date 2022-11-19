1ef5fe4c4aeed6f77b0865c541b88406f8fbc9f1
Daffa Ilhami <mdaffailhami@gmail.com>
2022-11-19 13:25:24 +0800

    Initialize

---
+++ lib/pages/home/pages/marketku/tambah_produk_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyTambahProdukButton extends StatelessWidget {
+  const MyTambahProdukButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: const Icon(Icons.add),
+    );
+  }
+}
